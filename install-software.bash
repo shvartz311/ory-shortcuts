6b3eef176f6aff8b6ce2cfb32b1ac1f76bfa3fc1
Joshua Novick <JDNovick@infiniteenergy.com>
2020-09-23 00:00:48 -0400

    Fix nvm,node,npm installation

--- install-software.bash
+++ install-software.bash
@@ -20,8 +20,7 @@ sudo apt-get install -y dotnet-sdk-3.1
 
 # oh-my-zsh - https://ohmyz.sh/
 sudo apt-get install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc --unattended
-chsh jdnovick -s /bin/zsh
+KEEP_ZSHRC=yes RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Enter password for changing default shell
 
 # I like mounting at /c instead of /mnt/c and this is part of that setup
@@ -29,7 +28,9 @@ sudo mkdir /c
 
 # nvm (npm, Node.js, etc.) - https://docs.microsoft.com/en-us/windows/nodejs/setup-on-wsl2#install-nvm-nodejs-and-npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-export NVM_DIR=/home/jdnovick/.nvm # This is unnecessary once you add nvm zsh plugin mentioned above. You can just reload after adding plugin instead of doing this line
+export NVM_DIR=/home/jdnovick/.nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
 nvm install --lts
 
