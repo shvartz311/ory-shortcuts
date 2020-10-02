a6ea69341943049aa369910462d25dff1092ae1f
Joshua Novick <JDNovick@infiniteenergy.com>
2020-10-02 10:12:11 -0400

    Cleanup after some testing

--- install-software.bash
+++ install-software.bash
@@ -13,6 +13,7 @@ sudo rm /usr/local/bin/kubectl
 # dotnet - https://docs.microsoft.com/en-us/dotnet/core/install/linux#ubuntu
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 sudo apt-get update
 # sudo apt-get install -y apt-transport-https && \
 # sudo apt-get update
