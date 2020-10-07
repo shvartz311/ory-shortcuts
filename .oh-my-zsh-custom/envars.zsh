38ec82cfba580f722fcb36d70709bd57d6e77a6c
Joshua Novick <JDNovick@infiniteenergy.com>
2020-10-07 11:50:48 -0400

    Improve certificate setup for npm and yarn

--- .oh-my-zsh-custom/envars.zsh
+++ .oh-my-zsh-custom/envars.zsh
@@ -2,5 +2,3 @@ export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 export NODE_EXTRA_CA_CERTS=/etc/ssl/certs/ca-certificates.crt
 DEFAULT_USER=jdnovick
 PROMPT_EOL_MARK=''
-
-export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
