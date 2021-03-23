5a1868ad1097d46b73c9c5561cc71fe1ec5ee544
Joshua Novick <joshua.novick@hrblock.com>
2021-03-23 16:53:42 -0500

    Add git changelog script
    * Autogenerates a changelog file based on git tags and commit messages

---
+++ .git-scripts/gitChangelog.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+BASE_URL=$(git remote get-url origin | sed "s;git@ssh.dev.azure.com:v3/hrblock/;https://dev.azure.com/hrblock;")
+
+echo "# Changelog" > CHANGELOG.md
+
+for TAG in $(git tag -l | tac)
+do
+  echo >> CHANGELOG.md
+  PREVIOUS_TAG=$(git describe --abbrev=0 $TAG^ 2> /dev/null || git rev-list --max-parents=0 HEAD | tail -n 1)
+  git tag -l --format="## [%(refname:strip=2)]($BASE_URL?version=GT%(refname:strip=2)) - %(contents)" $TAG >> CHANGELOG.md
+
+  printf "\n**Commits**:\n\n" >> CHANGELOG.md
+  git log --pretty="* [%h]($BASE_URL/commit/%H) - %s" $PREVIOUS_TAG..$TAG >> CHANGELOG.md
+done
