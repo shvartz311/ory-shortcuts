03c356fbc77b0f31fa6c50d7794ef85b7dd4f697
Joshua Novick <j.novick@ext.tricentis.com>
2022-01-20 18:39:42 +0200

    Add useful aliases/functions for working on ticket
    * Allow easily opening jira tickets and PRs
    * Also add a powerline symbol printer
    * Add quick alias for opening VS Code

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -9,3 +9,5 @@ alias unvpn='sudo ~/un-vpn-fix.bash'
 alias pip='pip3'
 
 alias code='code-insiders'
+alias dotfiles='code ~/dotfiles/'
+alias jira='explorer.exe "https://jira.tricentis.com/browse/`git rev-parse --abbrev-ref HEAD`"'
--- .oh-my-zsh-custom/misc.zsh
+++ .oh-my-zsh-custom/misc.zsh
@@ -22,3 +22,39 @@ git_remote_branch() {
   done
   echo master
 }
+
+pr() {
+  local URL="https://tricentis.visualstudio.com/"
+  URL="$URL$(pwd | sed "s|$HOME\/git\/\([^/]*\).*|\1|g")/_git/"
+  URL="$URL$(pwd | sed "s|$HOME/git/[^/]*/\([^/]*\)/.*|\1|g")/"
+  URL="${URL}pullrequestcreate?sourceRef=$(git rev-parse --abbrev-ref HEAD)&"
+  URL="${URL}targetRef=$(git_remote_branch)"
+
+  echo $URL
+}
+
+print_powerline_characters(){
+  for c4 in {0..9} {a..f}; do
+    for c3 in {a..d}; do
+      echo -n "\\\ue0${c3}${c4} - \\ue0${c3}${c4}    "
+    done
+    echo
+  done
+}
+
+  # '\ue0a0' -     '\ue0b0' -     '\ue0c0' -     '\ue0d0' - 
+  # '\ue0a1' -     '\ue0b1' -     '\ue0c1' -     '\ue0d1' - 
+  # '\ue0a2' -     '\ue0b2' -     '\ue0c2' -     '\ue0d2' - 
+  # '\ue0a3' -     '\ue0b3' -     '\ue0c3' - 
+  #                 '\ue0b4' -     '\ue0c4' -     '\ue0d4' - 
+  #                 '\ue0b5' -     '\ue0c5' - 
+  #                 '\ue0b6' -     '\ue0c6' - 
+  #                 '\ue0b7' -     '\ue0c7' - 
+  #                 '\ue0b8' -     '\ue0c8' - 
+  #                 '\ue0b9' - 
+  #                 '\ue0ba' -     '\ue0ca' - 
+  #                 '\ue0bb' - 
+  #                 '\ue0bc' -     '\ue0cc' - 
+  #                 '\ue0bd' -     '\ue0cd' - 
+  #                 '\ue0be' -     '\ue0ce' - 
+  #                 '\ue0bf' -     '\ue0cf' - 
